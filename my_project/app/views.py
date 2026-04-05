2e2a1700ad9588c5bddcf8458abdfd4f348b49a4
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-04-05 20:12:18 -0300

    Agregar nuevo proyecto my_project con app

---
+++ my_project/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.views import APIView  # Sin alias
+
+from app.models import User, Role
+
+# Create your views here.
